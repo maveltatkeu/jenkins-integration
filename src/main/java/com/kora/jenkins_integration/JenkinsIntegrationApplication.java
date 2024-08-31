1e2aadd4a394884f24e6dca95587c2ba5bf9717b
mavel.tatkeu.mbiakop <mavel.tatkeu.mbiakop@accenture.com>
2024-08-31 21:44:39 +0400

    Fis Commit

---
+++ src/main/java/com/kora/jenkins_integration/JenkinsIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.kora.jenkins_integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kora/jenkins_integration/JenkinsIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kora.jenkins_integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
